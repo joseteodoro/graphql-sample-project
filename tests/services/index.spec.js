13bff0b50311c990265928db666668aa47025c19
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2021-09-25 13:30:57 -0300

    adding migrations for integration tests

--- tests/services/index.spec.js
+++ tests/services/index.spec.js
@@ -1,6 +1,7 @@
 describe('src/listing tickets suite', () => {
   describe('Given graphql is working', () => {
     it('when I list all tickets it works properly', () => {
+      console.log(`>>>>>>>>>>>>`, process.env.NODE_ENV);
       return Promise.resolve(true);
     });
   });
