1fa42dd8ed32a285defa8e98bc902486a4e08e19
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2021-09-26 11:13:56 -0300

    calling full children functions

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -3,37 +3,37 @@ const service = require('../services');
 
 const resolvers = {
   Query: {
-    tickets: async () => repository.listAll().then(service.fillChildren), // should add all childs in the result recursively!
-    ticket: async (_, { id }) => repository.findBy({ id }).then(service.fillChildren), // also should add all childs in the result recursively!
+    tickets: async () => repository.listAll().then(service.populateAllChildren), // should add all childs in the result recursively!
+    ticket: async (_, { id }) => repository.findBy({ id }).then(service.populateAllChildren), // also should add all childs in the result recursively!
   },
   Ticket: {},
   Mutation: {
     createTicket: async (_, { title, isCompleted = false }) => {
       return repository.create({ title, isCompleted })
-        .then(service.fillChildren);
+        .then(service.populateAllChildren);
     },
     updateTicket: async (_, { id, title }) => {
       return repository.update(id, { title })
-        .then(service.fillChildren);
+        .then(service.populateAllChildren);
     },
     toggleTicket: async (_, { id, isCompleted }) => {
       return repository.update(id, { isCompleted })
-        .then(service.fillChildren);
+        .then(service.populateAllChildren);
     },
     setParentOfTicket: async (_, { parentId, childId }) => {
       return repository.update(childId, { parentId })
-        .then(service.fillChildren);
+        .then(service.populateAllChildren);
     },
     removeTicket: async (_, { id }) => {
       return repository.remove({ id });
     },
     removeParentFromTicket: async (_, { id }) => {
       return repository.removeParent({ id })
-        .then(service.fillChildren);
+        .then(service.populateAllChildren);
     },
     addChildrenToTicket: async (_, { id, childrenIds }) => {
       return repository.updateParent({ parentId: id, childrenIds })
-        .then(service.fillChildren);
+        .then(service.populateAllChildren);
     },
   },
 };
